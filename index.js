01c9f1cc157548f7adeafdccfe8e054933420816
Felix Kojo <felixkwadwo300@gmail.com>
2024-07-02 15:43:32 +0000

    Added event route

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-import express from "express";
+import express, { Router } from "express";
+import eventRouter from "./routes/event_route.js";
 
 const app = express()
+
+
+
+app.use(express.json())
+
+app.use = (eventRouter)
+
+
 
 const port = process.env.PORT || 6432;
 app.listen(port, () => {
