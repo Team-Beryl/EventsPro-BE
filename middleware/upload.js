e6571a51606d1e6169f990d37b84dca7d4c2c2be
Felix Kojo <felixkwadwo300@gmail.com>
2024-07-03 17:15:18 +0000

    disabled express

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,9 +3,9 @@ import { multerSaveFilesOrg } from "multer-savefilesorg";
 
 export const localUploads = multer({ dest: 'uploads' });
 
-export const remoteUpload = multer({
-    storage: multerSaveFilesOrg({
-        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
-        relativePath: '/uploads/*'
-    })
-})
+// export const remoteUpload = multer({
+//     storage: multerSaveFilesOrg({
+//         apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+//         relativePath: '/uploads/*'
+//     })
+// })
